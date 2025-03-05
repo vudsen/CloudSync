8461d4a449c6b814f31d636203b0dc6fef22253e
IceOfSummer <xu2237803016@outlook.com>
2025-03-05 11:05:05 +0800

    chore: 升级依赖版本

--- src/shared/dev/iframe-setup.ts
+++ src/shared/dev/iframe-setup.ts
@@ -1,24 +1,5 @@
-import type { MessagePoster } from 'cross-iframe-rpc'
-import { createBridePeerClientWithTypeOnly } from 'cross-iframe-rpc'
+import { setupInIframe } from 'cross-iframe-rpc'
 
 export default function iframeSetup() {
-  if (process.env.NODE_ENV === 'development') {
-    const poster: MessagePoster = {
-      postMessage(str) {
-        window.parent.window.postMessage(str, '*')
-      },
-      addEventListener(name, callback) {
-        window.addEventListener(name, (evt) => {
-          callback(evt)
-        })
-      },
-      removeEventListener(name, callback) {
-        window.removeEventListener(name, callback)
-      }
-    }
-    window.chrome = createBridePeerClientWithTypeOnly<typeof chrome>({
-      poster
-    })
-  }
-
+  window.chrome = setupInIframe<typeof chrome>()
 }
