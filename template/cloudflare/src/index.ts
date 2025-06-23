90c81749068ceb5486f1286eea6ccab92c7a5de4
IceOfSummer <xu2237803016@outlook.com>
2025-06-23 10:55:20 +0800

    使用官方模板

--- template/cloudflare/src/index.ts
+++ template/cloudflare/src/index.ts
@@ -1,14 +1,21 @@
-import type { ExportedHandler,Response as CfResponse } from '@cloudflare/workers-types'
 import crypto from 'node:crypto'
+import { Buffer } from 'node:buffer'
 
-interface Env {
-  AES_SECRET: string
-  AES_IV: string
-}
-
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
 
 export default {
-  async fetch(request, env): Promise<CfResponse> {
+  async fetch(request, env): Promise<Response> {
     const searchParams = new URL(request.url).searchParams
     const accountId = searchParams.get('accountId')
     const namespaceId = searchParams.get('namespaceId')
@@ -19,13 +26,13 @@ export default {
     const decipher = crypto.createDecipheriv('aes-256-cbc', env.AES_SECRET, iv)
     let decryptedApiToken = decipher.update(encryptedApiToken, 'utf-8', 'utf-8')
     decryptedApiToken += decipher.final('utf-8')
-    
+
     const resp = await fetch(`https://api.cloudflare.com/client/v4/accounts/${accountId}/storage/kv/namespaces/${namespaceId}/values/${name}`, {
       method: request.method,
       body: await request.text(),
       headers: {
         'Authorization': decryptedApiToken,
-        'Content-Type': request.headers.get('Content-Type'),
+        'Content-Type': request.headers.get('Content-Type') ?? 'application/json',
       }
     })
     return new Response(await resp.text(), {
@@ -34,6 +41,6 @@ export default {
       headers: {
         'Access-Control-Allow-Origin': '*',
       }
-    }) as unknown as CfResponse
+    })
   },
 } satisfies ExportedHandler<Env>
